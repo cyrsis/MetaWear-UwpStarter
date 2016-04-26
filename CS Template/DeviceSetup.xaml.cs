18f8654e758840c03993a269318e5d006317fbeb
etsai <etsai2@gmail.com>
2016-04-25 18:19:18 -0700

    Moved library initialization to MainPage

--- CS Template/DeviceSetup.xaml.cs
+++ CS Template/DeviceSetup.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MbientLab.MetaWear.Core;
-using MbientLab.MetaWear.Processor;
+﻿using MbientLab.MetaWear.Peripheral;
 using static MbientLab.MetaWear.Functions;
 using System;
 using System.Collections.Generic;
@@ -18,100 +17,30 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace MbientLab.MetaWear.Template {
     /// <summary>
-    /// Sets up the C++ API to be used in C# following the steps outlined in the C++ documentation:
-    /// https://mbientlab.com/cppdocs/latest/btlecommunication.html
+    /// Blank page where users add their MetaWear commands
     /// </summary>
     public sealed partial class DeviceSetup : Page {
         /// <summary>
-        /// Selected Bluetooth LE device from the paired devices list
-        /// </summary>
-        private BluetoothLEDevice selectedDevice;
-        /// <summary>
-        /// Delegate wrapper the <see cref="initialized"/> callback function
-        /// </summary>
-        private FnVoid initDelegate;
-        /// <summary>
-        /// Pointer representing the MblMwMetaWearBoard struct created by the C++ API
+        /// Pointer representing the MblMwMetaWearBoard struct passed in from the MainPage class
         /// </summary>
         private IntPtr board;
-        /// <summary>
-        /// C# wrapper around the MblMwBtleConnection struct 
-        /// </summary>
-        private BtleConnection btleConn;
+        
 
         public DeviceSetup() {
             this.InitializeComponent();
-
-            btleConn = new BtleConnection();
-            btleConn.writeGattChar = new FnVoidPtrByteArray(writeCharacteristic);
-            btleConn.readGattChar = new FnVoidPtr(readCharacteristic);
-
-            initDelegate = new FnVoid(initialized);
         }
-
-        /// <summary>
-        /// Callback function executed when the `mbl_mw_metawearboard_initialize` function is finished
-        /// </summary>
-        /// <remarks>Do not use the <see cref="board"/> variable until this function is called</remarks>
-        /// <seealso cref="mbl_mw_metawearboard_initialize"/>
-        private void initialized() {
-            // Configure your board here
-            System.Diagnostics.Debug.WriteLine("API Initialized");
-        }
-
-        /// <summary>
-        /// Writes a value to a GATT characteristic
-        /// </summary>
-        /// <param name="gattCharPtr">Pointer to a <see cref="MbientLab.MetaWear.Core.GattCharacteristic"/></param>
-        /// <param name="value">Pointer to a byte array containing the value</param>
-        /// <param name="length">Number of bytes</param>
-        private async void writeCharacteristic(IntPtr gattCharPtr, IntPtr value, byte length) {
-            byte[] managedArray = new byte[length];
-            Marshal.Copy(value, managedArray, 0, length);
-
-            var charGuid = Marshal.PtrToStructure<MbientLab.MetaWear.Core.GattCharacteristic>(gattCharPtr).toGattCharGuid();
-            var status = await selectedDevice.GetGattService(charGuid.serviceGuid).GetCharacteristics(charGuid.guid).FirstOrDefault()
-                .WriteValueAsync(managedArray.AsBuffer(), GattWriteOption.WriteWithoutResponse);
-
-            if (status != GattCommunicationStatus.Success) {
-                System.Diagnostics.Debug.WriteLine("Error writing gatt characteristic");
-            }
-        }
-        /// <summary>
-        /// Reads the value from a GATT characteristic
-        /// </summary>
-        /// <param name="gattCharPtr">Pointer to a <see cref="MbientLab.MetaWear.Core.GattCharacteristic"/></param>
-        private async void readCharacteristic(IntPtr gattCharPtr) {
-            var charGuid = Marshal.PtrToStructure<MbientLab.MetaWear.Core.GattCharacteristic>(gattCharPtr).toGattCharGuid();
-            var result = await selectedDevice.GetGattService(charGuid.serviceGuid).GetCharacteristics(charGuid.guid).FirstOrDefault()
-                .ReadValueAsync();
-
-            if (result.Status == GattCommunicationStatus.Success) {
-                mbl_mw_connection_char_read(board, gattCharPtr, result.Value.ToArray(), (byte)result.Value.Length);
-            } else {
-                System.Diagnostics.Debug.WriteLine("Error reading gatt characteristic");
-            }
-        }
-
-        protected override async void OnNavigatedTo(NavigationEventArgs e) {
+        
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
             base.OnNavigatedTo(e);
 
-            selectedDevice = e.Parameter as BluetoothLEDevice;
-            var notifyChar = selectedDevice.GetGattService(GattCharGuid.METAWEAR_NOTIFY_CHAR.serviceGuid).GetCharacteristics(GattCharGuid.METAWEAR_NOTIFY_CHAR.guid).FirstOrDefault();
-            await notifyChar.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
-            notifyChar.ValueChanged += new TypedEventHandler<Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristic, GattValueChangedEventArgs>(
-                (Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristic sender, GattValueChangedEventArgs obj) => {
-                    byte[] response = obj.CharacteristicValue.ToArray();
-                    mbl_mw_connection_notify_char_changed(board, response, (byte)response.Length);
-                });
-
-            board = mbl_mw_metawearboard_create(ref btleConn);
-            mbl_mw_metawearboard_initialize(board, initDelegate);
+            board = (IntPtr) e.Parameter;
         }
 
         private void back_Click(object sender, RoutedEventArgs e) {
--- CS Template/MainPage.xaml.cs
+++ CS Template/MainPage.xaml.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using MbientLab.MetaWear.Core;
+using MbientLab.MetaWear.Processor;
+using static MbientLab.MetaWear.Functions;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -50,13 +57,37 @@ namespace MbientLab.MetaWear.Template {
     }
 
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Sets up the C++ API to be used in C# following the steps outlined in the C++ documentation:
+    /// https://mbientlab.com/cppdocs/latest/btlecommunication.html
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        /// <summary>
+        /// Pointer representing the MblMwMetaWearBoard struct created by the C++ API
+        /// </summary>
+        private IntPtr board;
+        /// <summary>
+        /// Delegate wrapper the <see cref="initialized"/> callback function
+        /// </summary>
+        private FnVoid initDelegate;
+        /// <summary>
+        /// Selected Bluetooth LE device from the paired devices list
+        /// </summary>
+        private BluetoothLEDevice selectedDevice;
+        /// <summary>
+        /// C# wrapper around the MblMwBtleConnection struct 
+        /// </summary>
+        private BtleConnection btleConn;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            btleConn = new BtleConnection();
+            btleConn.writeGattChar = new FnVoidPtrByteArray(writeCharacteristic);
+            btleConn.readGattChar = new FnVoidPtr(readCharacteristic);
+
+            initDelegate = new FnVoid(initialized);
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e) {
@@ -72,11 +103,71 @@ namespace MbientLab.MetaWear.Template {
             }
         }
 
-        private void pairedDevices_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            BluetoothLEDevice selected = ((ListView)sender).SelectedItem as BluetoothLEDevice;
+        private async void pairedDevices_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            selectedDevice = ((ListView)sender).SelectedItem as BluetoothLEDevice;
 
-            if (selected != null) {
-                this.Frame.Navigate(typeof(DeviceSetup), selected);
+            if (selectedDevice != null) {
+                initFlyout.ShowAt(pairedDevices);
+
+                var notifyChar = selectedDevice.GetGattService(GattCharGuid.METAWEAR_NOTIFY_CHAR.serviceGuid).GetCharacteristics(GattCharGuid.METAWEAR_NOTIFY_CHAR.guid).FirstOrDefault();
+                await notifyChar.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                notifyChar.ValueChanged += new TypedEventHandler<Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristic, GattValueChangedEventArgs>(
+                    (Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristic gattCharChanged, GattValueChangedEventArgs obj) => {
+                        byte[] response = obj.CharacteristicValue.ToArray();
+                        mbl_mw_connection_notify_char_changed(board, response, (byte)response.Length);
+                    });
+
+                board = mbl_mw_metawearboard_create(ref btleConn);
+                mbl_mw_metawearboard_initialize(board, initDelegate);
+            }
+        }
+
+        /// <summary>
+        /// Callback function executed when the `mbl_mw_metawearboard_initialize` function is finished
+        /// </summary>
+        /// <remarks>Do not use the <see cref="board"/> variable until this function is called</remarks>
+        /// <seealso cref="mbl_mw_metawearboard_initialize"/>
+        private async void initialized() {
+            // Configure your board here
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal, () => {
+                    initFlyout.Hide();
+                    this.Frame.Navigate(typeof(DeviceSetup), board);
+                }
+            );
+        }
+
+        /// <summary>
+        /// Writes a value to a GATT characteristic
+        /// </summary>
+        /// <param name="gattCharPtr">Pointer to a <see cref="MbientLab.MetaWear.Core.GattCharacteristic"/></param>
+        /// <param name="value">Pointer to a byte array containing the value</param>
+        /// <param name="length">Number of bytes</param>
+        private async void writeCharacteristic(IntPtr gattCharPtr, IntPtr value, byte length) {
+            byte[] managedArray = new byte[length];
+            Marshal.Copy(value, managedArray, 0, length);
+
+            var charGuid = Marshal.PtrToStructure<MbientLab.MetaWear.Core.GattCharacteristic>(gattCharPtr).toGattCharGuid();
+            var status = await selectedDevice.GetGattService(charGuid.serviceGuid).GetCharacteristics(charGuid.guid).FirstOrDefault()
+                .WriteValueAsync(managedArray.AsBuffer(), GattWriteOption.WriteWithoutResponse);
+
+            if (status != GattCommunicationStatus.Success) {
+                System.Diagnostics.Debug.WriteLine("Error writing gatt characteristic");
+            }
+        }
+        /// <summary>
+        /// Reads the value from a GATT characteristic
+        /// </summary>
+        /// <param name="gattCharPtr">Pointer to a <see cref="MbientLab.MetaWear.Core.GattCharacteristic"/></param>
+        private async void readCharacteristic(IntPtr gattCharPtr) {
+            var charGuid = Marshal.PtrToStructure<MbientLab.MetaWear.Core.GattCharacteristic>(gattCharPtr).toGattCharGuid();
+            var result = await selectedDevice.GetGattService(charGuid.serviceGuid).GetCharacteristics(charGuid.guid).FirstOrDefault()
+                .ReadValueAsync();
+
+            if (result.Status == GattCommunicationStatus.Success) {
+                mbl_mw_connection_char_read(board, gattCharPtr, result.Value.ToArray(), (byte)result.Value.Length);
+            } else {
+                System.Diagnostics.Debug.WriteLine("Error reading gatt characteristic");
             }
         }
     }
